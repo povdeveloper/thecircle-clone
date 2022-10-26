aba82e8baca01d3cd48e9ba7230f6031bf3c84a2
Sergio Kepo <116714435+povdeveloper@users.noreply.github.com>
2022-10-26 12:46:07 +0200

    feat: Added Background

---
+++ src/components/background/Background.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './background.style.css'
+
+function Background() {
+  return (
+    <div className='the-circle'>
+    <div className='circle rotating'></div>
+
+    </div>
+  )
+}
+
+export default Background

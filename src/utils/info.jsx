ff2d2d27b3b4fa858c9d980a8b75532c7bf0ecdf
Sergio Kepo <116714435+povdeveloper@users.noreply.github.com>
2022-10-26 12:45:28 +0200

    Batman!

---
+++ src/utils/info.jsx
@@ -0,0 +1,41 @@
+export const contestantInfo = [
+    {
+        name: 'Bryant',
+        image: 'https://firebasestorage.googleapis.com/v0/b/povdeveloper-97eab.appspot.com/o/thecircle-clone%2Fprofile%2Fbryant.png?alt=media&token=dade9fc5-68b4-4fbc-bd3e-200e70d6c484'
+    },
+    {
+        name: 'Chloe',
+        image: 'https://firebasestorage.googleapis.com/v0/b/povdeveloper-97eab.appspot.com/o/thecircle-clone%2Fprofile%2Fchloe.png?alt=media&token=f1364f71-0d59-402c-a124-b6915d2ef1d9'
+
+    },
+    {
+        name: 'Courtney',
+        image: 'https://firebasestorage.googleapis.com/v0/b/povdeveloper-97eab.appspot.com/o/thecircle-clone%2Fprofile%2Fcourtney.png?alt=media&token=339abfdf-d34a-470c-941a-18ffabb2fd6c'
+
+    },
+    {
+        name: 'Emily',
+        image: 'https://firebasestorage.googleapis.com/v0/b/povdeveloper-97eab.appspot.com/o/thecircle-clone%2Fprofile%2Femily.png?alt=media&token=b805cf4a-e443-4e7d-8b2a-ea8d66df4350'
+
+    },
+    {
+        name: 'River',
+        image: 'https://firebasestorage.googleapis.com/v0/b/povdeveloper-97eab.appspot.com/o/thecircle-clone%2Fprofile%2Friver.png?alt=media&token=cfab353b-8a28-49fe-95a7-3e95fdf1b4a4'
+
+    },
+    {
+        name: 'Savannah',
+        image: 'https://firebasestorage.googleapis.com/v0/b/povdeveloper-97eab.appspot.com/o/thecircle-clone%2Fprofile%2Fsavannah.png?alt=media&token=639ad89d-899b-45f6-9bf5-f52365432cd9'
+
+    },
+    {
+        name: 'Terilisha',
+        image: 'https://firebasestorage.googleapis.com/v0/b/povdeveloper-97eab.appspot.com/o/thecircle-clone%2Fprofile%2Fterilisha.png?alt=media&token=e167d375-736f-496c-bdcc-50d119f59c7a'
+
+    },
+    {
+        name: 'Trevor',
+        image: 'https://firebasestorage.googleapis.com/v0/b/povdeveloper-97eab.appspot.com/o/thecircle-clone%2Fprofile%2Ftrevor.png?alt=media&token=01ad5787-b313-487f-bfa4-a29d82a4425f'
+
+    },
+]
